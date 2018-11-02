acac90ede15c3380d99f3b11d2ab4de969e37767
Olivia Parker <oliviaparkerva@gmail.com>
2018-11-01 23:27:49 -0400

    Book,shelf
    Created book and shelf components main page functional

--- src/Components/shelves.js
+++ src/Components/shelves.js
@@ -1,9 +1,21 @@
-//bookshelf
-iv className="bookshelf">
-                  <h2 className="bookshelf-title">Currently Reading</h2>
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
-                      //insert books component here
-                    </ol>
-                  </div>
-                </div>
+import React from 'react';
+import Book from './Books';
+
+
+class Shelf extends React.Component{
+
+  render(){
+    return(
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">Currently Reading</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+            <Book />
+          </ol>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Shelf
