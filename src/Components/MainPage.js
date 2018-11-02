ddb36140782f6f8bffd076e339b749877643b16a
Olivia Parker <oliviaparkerva@gmail.com>
2018-11-02 00:58:02 -0400

    BOOKS on shelves on Main
    Got my books component to populate into the shelves with the correct authors name, titles and images!!

--- src/Components/MainPage.js
+++ src/Components/MainPage.js
@@ -1,15 +1,28 @@
 //Main Page
 //imports: React; link for search button;booksapi to get list of searchable books(getAll),update books to add them to the main page shelves, search to search through books available to add them to the list,shelf(contains book objects)
 //export this page to be used in App.js and search.js
-//objects:
+//componentDidMount go through all the books available using getAll method
+//react constructor method and super https://is.gd/4KzZvb or UDACITY lesson on setting state with newly proposed method https://is.gd/4KzZvb
 
 import React from 'react';
 import{ Link } from 'react-router-dom';
+import * as BooksAPI from '../BooksAPI';
 import Shelf from './Shelves';
 
 
 class MainPage extends React.Component{
 
+  state = {
+  	books:[]
+  }
+
+  componentDidMount(){
+    BooksAPI.getAll()
+    .then(resp => {
+      console.log(resp);
+      this.setState({books: resp})
+    });
+  }
 
   render(){
     return(
@@ -20,7 +33,9 @@ class MainPage extends React.Component{
         </div>
         <div className="list-books-content">
           <div>
-            <Shelf />
+            <Shelf name="Current Reads" books={this.state.books.filter(book => book.shelf === "currentlyReading")}/>
+	        <Shelf name="Want to Read" books={this.state.books.filter(book => book.shelf === "wantToRead")}/>
+            <Shelf name="Read" books={this.state.books.filter(book => book.shelf === "read")}/>
           </div>
         </div>
       </div>
--- src/Components/books.js
+++ src/Components/books.js
@@ -2,7 +2,7 @@
 //Imports: React. Export:Book
 //render>return(entire code for book)
 //Don't need to change bookshelfchanger
-//Book object
+//Book needs to access unique author, thumbnail, title
 import React from 'react';
 
 class Book extends React.Component{
@@ -12,7 +12,7 @@ class Book extends React.Component{
         <li>
         <div className="book">
           <div className="book-top">
-            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
+            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${this.props.book.imageLinks.thumbnail}")` }}></div>
             <div className="book-shelf-changer">
               <select>
                 <option value="move" disabled>Move to...</option>
@@ -23,28 +23,11 @@ class Book extends React.Component{
               </select>
             </div>
           </div>
-          <div className="book-title">To Kill a Mockingbird</div>
-          <div className="book-authors">Harper Lee</div>
+          <div className="book-title">{this.props.book.title}</div>
+          <div className="book-authors">{this.props.book.authors}</div>
         </div>
       </li>
-      <li>
-        <div className="book">
-          <div className="book-top">
-            <div className="book-cover" style={{ width: 128, height: 188, backgroundImage: 'url("http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api")' }}></div>
-            <div className="book-shelf-changer">
-              <select>
-                <option value="move" disabled>Move to...</option>
-                <option value="currentlyReading">Currently Reading</option>
-                <option value="wantToRead">Want to Read</option>
-                <option value="read">Read</option>
-                <option value="none">None</option>
-              </select>
-            </div>
-          </div>
-          <div className="book-title">Ender's Game</div>
-          <div className="book-authors">Orson Scott Card</div>
-        </div>
-      </li>
+
       </div>
 
     )
